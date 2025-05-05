788f7b181f91a2957c9ea1e3302acc9bf0517b11
AritzAyensa <aayensaeli1@educacion.navarra.es>
2025-05-05 19:45:08 +0200

    biuld gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
-}
-
-dependencies {
-    // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-
+    id("com.google.gms.google-services") // Necesario para Firebase
 }
 
 android {
@@ -33,47 +27,55 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
+    // Firebase BoM (gestiona versiones automáticamente)
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-auth")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
-    // BoM para Firebase
-    //implementation("com.google.firebase:firebase-firestore:25.1.1")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx")
 
+    // Coroutines para Firebase
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
+
+    // AR y otros
     implementation("com.google.ar.sceneform:filament-android:1.17.1")
     implementation("androidx.activity:activity:1.8.0")
 
-
-
-    /* DEFAULT */
+    // Android Jetpack y Material
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Navigation y Lifecycle
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    //AUTH FIREBASE
-    //implementation ("com.google.firebase:firebase-auth:21.0.1")
-    implementation ("com.google.android.gms:play-services-safetynet:18.0.0")
+    // SafetyNet (para Firebase Auth o seguridad)
+    implementation("com.google.android.gms:play-services-safetynet:18.0.0")
 
-    //Zxing
-    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
+    // QR
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
